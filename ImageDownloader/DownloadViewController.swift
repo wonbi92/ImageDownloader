1c90759b3cc4b7b9ff8c2bcf51e53acaf0f607d2
wonbi92 <bin9239@gmail.com>
2023-03-02 01:57:09 +0900

    feat: 메인 뷰 구현

--- ImageDownloader/DownloadViewController.swift
+++ ImageDownloader/DownloadViewController.swift
@@ -8,8 +8,48 @@
 import UIKit
 
 final class DownloadViewController: UIViewController {
+    private let imageLoadViews: [ImageLoadView] = ImageLoadViewFactory.makeImageLoadView(multiplier: 5)
+    
+    private let loadAllButton: UIButton = {
+        let button = UIButton(configuration: .filled())
+        button.setTitle("Load All Images", for: .normal)
+        return button
+    }()
+    
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+        configureLayout()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .systemBackground
+        
+        imageLoadViews.forEach(mainStackView.addArrangedSubview(_:))
+        mainStackView.addArrangedSubview(loadAllButton)
+        
+        view.addSubview(mainStackView)
+    }
+    
+    private func configureLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20)
+        ])
     }
 }
 
