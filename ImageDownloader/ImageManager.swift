29dc1084e2f282d83f8905e31e810623cd1ab63d
wonbi92 <bin9239@gmail.com>
2023-03-02 00:18:24 +0900

    feat: ImageManager 객체 생성

---
+++ ImageDownloader/ImageManager.swift
@@ -0,0 +1,16 @@
+//
+//  ImageManager.swift
+//  ImageDownloader
+//
+//  Created by Wonbi on 2023/03/01.
+//
+
+import UIKit
+
+final class ImageManager {
+    private let networkManager: NetworkManager
+    
+    init(networkManager: NetworkManager) {
+        self.networkManager = networkManager
+    }
+}
