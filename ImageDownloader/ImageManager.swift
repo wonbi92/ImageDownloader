ecdc01365a03958c3348708d1afdf624481cd788
wonbi92 <bin9239@gmail.com>
2023-03-02 00:18:57 +0900

    feat: ImageManager에 네트워크로부터 데이터를 받아 섬네일을 만드는 로직 추가

--- ImageDownloader/ImageManager.swift
+++ ImageDownloader/ImageManager.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
+enum ImageError: Error {
+    case badData, badImage
+}
+
 final class ImageManager {
     private let networkManager: NetworkManager
     
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
+    }
+    
+    func createThumbnail(for index: Int, completion: @escaping (Result<UIImage, Error>) -> Void) {
+        networkManager.fetchData(for: index) { result in
+            switch result {
+            case .success(let data):
+                guard let image = UIImage(data: data) else {
+                    completion(.failure(ImageError.badData))
+                    return
+                }
+                
+                image.prepareThumbnail(of: CGSize(width: 160, height: 90)) { thumbnail in
+                    guard let thumbnail = thumbnail else {
+                        completion(.failure(ImageError.badImage))
+                        return
+                    }
+                    completion(.success(thumbnail))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
     }
 }
